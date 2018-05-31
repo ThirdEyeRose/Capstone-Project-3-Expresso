87f52e610f1a65e50520790c6cb3e5acd0996db3
Pat East <that-pat@github.com>
2018-05-31 15:55:22 -0400

    Implement POST /api/employees route
    (needs refactoring)

--- employeeRouter.js
+++ employeeRouter.js
@@ -4,6 +4,19 @@ const sqlite3 = require('sqlite3');
 const employeeRouter = express.Router();
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+function validateEmployeeInput(req, res, next) {
+  let employee = req.body.employee;
+  if (employee === undefined ||
+    !employee.name ||
+    !employee.position ||
+    !employee.wage) {
+    res.status(400).send();
+  } else {
+    req.employee = employee;
+    next();
+  }
+}
+
 employeeRouter.param('id', (req, res, next, id) => {
   db.get(`SELECT * FROM Employee WHERE id = ${id}`, (error, row) => {
     if (error) {
@@ -25,6 +38,24 @@ employeeRouter.get('/', (req, res, next) => {
 
 employeeRouter.get('/:id', (req, res, next) => {
   res.send({ employee: req.employee });
+});
+
+employeeRouter.post('/', validateEmployeeInput, (req, res, next) => {
+  db.run(`INSERT INTO Employee (name, position, wage)
+  VALUES ($name, $position, $wage)`, {
+    $name: req.employee.name,
+    $position: req.employee.position,
+    $wage: req.employee.wage
+  }, function (error) {
+      if (error) {
+        console.log(error);
+      } else {
+        db.get(`SELECT * FROM Employee
+          WHERE id = ${this.lastID}`, (error, row) => {
+            res.status(201).send({ employee: row });
+          });
+      }
+    });
 });
 
 module.exports = employeeRouter;
