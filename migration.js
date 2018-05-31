fa05ca34d245844abb6aa6527f257030265a2fae
Pat East <that-pat@github.com>
2018-05-31 13:00:31 -0400

    Create Table Timesheet

--- migration.js
+++ migration.js
@@ -3,6 +3,7 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 db.serialize( () => {
+  // Drop and Create Employee Table
   db.run(`DROP TABLE IF EXISTS Employee`);
   db.run(`CREATE TABLE Employee (
     id INTEGER PRIMARY KEY,
@@ -10,5 +11,16 @@ db.serialize( () => {
     position TEXT NOT NULL,
     wage INTEGER NOT NULL,
     is_current_employee INTEGER DEFAULT 1
+  )`);
+
+  // Drop and Create Timesheet Table
+  db.run(`DROP TABLE IF EXISTS Timesheet`);
+  db.run(`CREATE TABLE Timesheet (
+    id INTEGER PRIMARY KEY,
+    hours INTEGER NOT NULL,
+    rate INTEGER NOT NULL,
+    date INTEGER NOT NULL,
+    employee_id INTEGER NOT NULL,
+    FOREIGN KEY(employee_id) REFERENCES Employee(id)
   )`);
 });
