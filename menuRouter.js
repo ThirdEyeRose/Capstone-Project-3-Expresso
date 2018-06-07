e40eea94da41826c7babd2e5673b928b698506ff
Pat East <that-pat@github.com>
2018-06-07 11:38:39 -0400

    Implement GET /api/menus

--- menuRouter.js
+++ menuRouter.js
@@ -4,4 +4,10 @@ const sqlite3 = require('sqlite3');
 const menuRouter = express.Router();
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuRouter.get('/', (req, res, next) => {
+  db.all(`SELECT * FROM Menu`, (error, rows) => {
+    res.send({ menus: rows });
+  });
+});
+
 module.exports = menuRouter;
