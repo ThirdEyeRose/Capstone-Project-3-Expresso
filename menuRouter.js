32e838276739d8545eb23e38c31fdf31ac020014
Pat East <that-pat@github.com>
2018-06-07 12:09:42 -0400

    Implement POST /api/menus

--- menuRouter.js
+++ menuRouter.js
@@ -4,6 +4,17 @@ const sqlite3 = require('sqlite3');
 const menuRouter = express.Router();
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+function validateMenuInput(req, res, next) {
+  let menu = req.body.menu;
+  if (menu === undefined ||
+    !menu.title ) {
+    res.status(400).send();
+  } else {
+    req.menuInput = menu;
+    next();
+  }
+}
+
 menuRouter.param('id', (req, res, next, id) => {
   db.get(`SELECT * FROM Menu WHERE id = ${id}`, (error, row) => {
     if (error) {
@@ -25,6 +36,21 @@ menuRouter.get('/', (req, res, next) => {
 
 menuRouter.get('/:id', (req, res, next) => {
   res.send({ menu: req.menu });
+});
+
+menuRouter.post('/', validateMenuInput, (req, res, next) => {
+  db.run(`INSERT INTO Menu (title) VALUES ($title)`, {
+    $title: req.menuInput.title
+  }, function (error) {
+      if (error) {
+        console.log(error);
+      } else {
+        db.get(`SELECT * FROM Menu
+          WHERE id = ${this.lastID}`, (error, row) => {
+            res.status(201).send({ menu: row });
+          });
+      }
+    });
 });
 
 module.exports = menuRouter;
